6da84c1488a1de06f7be11e0296e1e4528cb0001
amalle <alassanemalle733@gmail.com>
2022-08-26 16:55:20 +0000

    first message

---
+++ src/main/java/com/apierrornotes/ApiErrorNotes/ApiErrorNotesApplication.java
@@ -0,0 +1,13 @@
+package com.apierrornotes.ApiErrorNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErrorNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiErrorNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apierrornotes/ApiErrorNotes/ApiErrorNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apierrornotes.ApiErrorNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErrorNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
