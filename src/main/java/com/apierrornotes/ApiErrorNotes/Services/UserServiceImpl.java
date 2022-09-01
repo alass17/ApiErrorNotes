097b55ba95f1a2bfc39bcbd2e8362374f9b0bbaf
amalle <alassanemalle733@gmail.com>
2022-09-01 12:18:38 +0000

    Verification de compte

--- src/main/java/com/apierrornotes/ApiErrorNotes/Services/UserServiceImpl.java
+++ src/main/java/com/apierrornotes/ApiErrorNotes/Services/UserServiceImpl.java
@@ -3,6 +3,10 @@ package com.apierrornotes.ApiErrorNotes.Services;
 import com.apierrornotes.ApiErrorNotes.Models.User;
 import com.apierrornotes.ApiErrorNotes.Repository.UserRepo;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,14 +18,25 @@ public class UserServiceImpl implements UserService {
     private final UserRepo userRepo;
 
 
+
+ PasswordEncoder passwordEncoder() {
+     return new BCryptPasswordEncoder();
+ }
+
     //Cette condition nous permet de verifier par email si utilisateur exixte dans la base de donnée
     @Override
     public String creer(User user) {
+
         if(userRepo.existsByEmail(user.getEmail())){
+
             return "Cet utilisateur existe deja !!";
         }
-        userRepo.save(user);
-        return "Utilisateur enregistré !!";
+        else {
+            user.setMdp(passwordEncoder().encode(user.getMdp()));
+            userRepo.save(user);
+            return "Utilisateur enregistré !!";
+        }
+
     }
 
     @Override
